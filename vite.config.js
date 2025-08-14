5efad3d74934ffb776f614203634419a3ab1537a
EduardoAlparov <workman133@yandex.ru>
2025-08-14 20:12:48 +0300

    fix paths

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,5 @@
-import { ViteWebfontDownload } from 'vite-plugin-webfont-dl';
-
-// import { dependencies } from './package.json';
-// function renderChunks(deps) { // https://vitejs.dev/config/
-//     let chunks = {};
-//     Object.keys(deps).forEach((key) => {
-//         if (['@fortawesome/fontawesome-free'].includes(key)) return;
-//         chunks[key] = [key];
-//     });
-//     return chunks;
-// }
+import path from 'path';
 
 export default {
-    // css: {
-    //     preprocessorOptions: {
-    //       scss: {
-    //         additionalData: `@use "/scss/mixins.scss";`,
-    //       },
-    //     },
-    // },
-    plugins: [
-        // ViteWebfontDownload([
-        //     'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap',
-        // ]),
-    ],
-    optimizeDeps: {
-        // exclude: ['@fortawesome/fontawesome-free'],
-    },
-    // build: {
-    //     // sourcemap: false,
-    //     rollupOptions: {
-    //         output: {
-    //             manualChunks: {
-    //                 vendor: ['@fortawesome/fontawesome-free'],
-    //                 ...renderChunks(dependencies),
-    //             },
-    //         },
-    //     },
-    // },
+    base: './',
 };
