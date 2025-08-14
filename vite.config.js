cc9a2d8ef3623e1308921fbcc03410609c8ccacf
EduardoAlparov <workman133@yandex.ru>
2025-08-14 20:15:20 +0300

    fix paths

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,18 @@ import path from 'path';
 
 export default {
     base: './',
+
+    plugins: [
+        // ViteWebfontDownload([
+        //     'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap',
+        // ]),
+    ],
+    optimizeDeps: {
+        // exclude: ['@fortawesome/fontawesome-free'],
+    },
+    build: {
+        compilerOptions: {
+            baseUrl: "./assets",
+        },
+    },
 };
